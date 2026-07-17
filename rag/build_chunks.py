4fc99be1b583ac9f2119bc08f9a8abba1c5bcc3c
Mantavya <themantavyasethi@gmail.com>
2026-07-17 09:12:12 -0400

    created roadmap to building the rag pipeline

---
+++ rag/build_chunks.py
@@ -0,0 +1,7 @@
+# things to do:
+# create an object that looks like: chunk_id, file_name, title, category, section, chunk_text, embedding
+# Use sentence transformer MiniLM to convert the documents in knowledge base to chunks
+# loop through each file and convert each section in the file to a chunk_id
+# Use regex to identify sections (split by section headings)
+# Create an embedding per section
+# store the embedding plus all the aforementioned data together
