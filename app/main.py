1e2734d8a03e56072a2ca58a1cb2164e489f8bc0
Mantavya <themantavyasethi@gmail.com>
2026-07-21 19:49:33 -0400

    Created a get endpoint which displays all tickets

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from app.schemas import TicketRequest, Priority, Category, TicketReview
 from app.predict import predict_ticket
 from app.insert_user_data import insert_data
 from app.update_ticket import update_data
+from app.get_tickets import get_all_tickets, get_specific_ticket
 from rag.retrieve import retrieve as similarity_search
 
 app = FastAPI()
@@ -53,3 +54,19 @@ def review_ticket(ticket_id: str, review: TicketReview):
             "confirmed_category": review.confirmed_category.value,
             "reviewed": True,
         }
+
+@app.get("/tickets")
+def get_tickets():
+    return get_all_tickets()
+
+@app.get("/tickets/{ticket_id}")
+def fetch_specific_ticket(ticket_id: str):
+    ticket = get_specific_ticket(ticket_id)
+
+    if ticket is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Ticket not found",
+        )
+    
+    return ticket
