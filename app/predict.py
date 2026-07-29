82962ccc82b40cc124ab3ead8fd3ba37a4e98dcf
Mantavya <themantavyasethi@gmail.com>
2026-07-29 15:14:03 -0400

    Edited the prediction function so it uses the models in s3 instead of local, added boto3 to requirements.txt

--- app/predict.py
+++ app/predict.py
@@ -1,10 +1,24 @@
 import joblib
 from pathlib import Path
+from infrastructure.s3_storage import download_file
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-MODEL_PATH = BASE_DIR / "models" / "ticket_classifier_split_v1.joblib"
+# BASE_DIR = Path(__file__).resolve().parent.parent
+# MODEL_PATH = BASE_DIR / "models" / "ticket_classifier_split_v1.joblib"
 
-classifier = joblib.load(MODEL_PATH)
+MODEL_BUCKET = os.environ["MODEL_S3_BUCKET"]
+MODEL_KEY = os.environ["MODEL_S3_KEY"]
+MODEL_AWS_PATH = Path(os.getenv("MODEL_AWS_PATH", "/tmp/ticket-classifier.joblib"))
+
+def load_model():
+    download_file(
+        bucket = MODEL_BUCKET,
+        key = MODEL_KEY,
+        local_path = MODEL_AWS_PATH
+    )
+    return joblib.load(MODEL_AWS_PATH)
+
+classifier = load_model()
+# classifier = joblib.load(MODEL_PATH)
 
 def predict_ticket(subcategory:str, description: str):
     text  = f"{subcategory} {description}"
