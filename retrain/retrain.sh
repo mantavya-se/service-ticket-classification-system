eb7b918181288402a068f217aafe7154aa0cd688
Mantavya <themantavyasethi@gmail.com>
2026-07-29 15:24:44 -0400

    Updated script to keep up with changes

--- retrain/retrain.sh
+++ retrain/retrain.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "Pulling new data from db and combining with synthetic dataset"
+cd /app
+
+echo "Downloading synthetic data and pulling new records from RDS"
 python3 -m retrain.retrain
 
 echo "Validating synthetic dataset"
 python3 -m retrain.validate
+
+echo "Uploading updated synthetic dataset to S3"
+python3 -m retrain.upload_outputs
+
+echo "Retraining data update completed"
