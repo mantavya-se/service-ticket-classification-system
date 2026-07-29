b7582ba09db1c7b22c16a76ed053a8e5e1e666ca
Mantavya <themantavyasethi@gmail.com>
2026-07-29 15:18:58 -0400

    Updated file paths and the workflow of the script: now downloads file from s3 into temp location, trains model, uploads files and model to s3

--- ml/combine_train.sh
+++ ml/combine_train.sh
@@ -2,11 +2,15 @@
 
 set -euo pipefail
 
-FILE1="data/public_it_tickets.csv"
-FILE2="data/synthetic_tickets.csv"
-OUTPUT_FILE="data/combined_ticket.csv"
+echo "Downloading training datasets from S3"
+cd /app
+python3 -m ml.download_files
 
-echo "Combining Files"
+FILE1="/tmp/data/public_it_tickets.csv"
+FILE2="/tmp/data/synthetic_tickets.csv"
+OUTPUT_FILE="/tmp/data/combined_ticket.csv"
+
+echo "Combining files"
 
 head -n 1 "$FILE1" > "$OUTPUT_FILE"
 tail -n +2 "$FILE1" >> "$OUTPUT_FILE"
@@ -14,6 +18,10 @@ tail -n +2 "$FILE2" >> "$OUTPUT_FILE"
 
 echo "Combined dataset created at: $OUTPUT_FILE"
 
-echo "Starting Model Training"
-cd /app
+echo "Starting model training"
 python3 -m ml.train
+
+echo "Uploading generated files to S3"
+python3 -m ml.upload_files
+
+echo "Training pipeline completed successfully"
